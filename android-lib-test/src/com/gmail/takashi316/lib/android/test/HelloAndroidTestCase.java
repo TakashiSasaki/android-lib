47f316dd2d93cb5aa6c6f4933a53bf37d8bcd650
Takashi SASAKI <takashi316@gmail.com>
2012-06-28 09:37:26 +0900

    android-lib-test targets HelloAndroid (com.gmail.takashi316.sandbox.HelloAndroid)

---
+++ android-lib-test/src/com/gmail/takashi316/lib/android/test/HelloAndroidTestCase.java
@@ -0,0 +1,51 @@
+package com.gmail.takashi316.lib.android.test;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import android.widget.TextView;
+import com.gmail.takashi316.sandbox.R;
+
+import com.gmail.takashi316.sandbox.HelloAndroidActivity;
+
+/**
+ * @author Takashi SASAKI {@link "http://twitter.com/TakashiSasaki"}
+ */
+public class HelloAndroidTestCase extends
+		ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+	public HelloAndroidTestCase() {
+		super(HelloAndroidActivity.class);
+	}// HelloAndroidTestCase
+
+	public void testTextViewHello() {
+		this.activity.runOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+				getInstrumentation().waitForIdle(null);
+			}
+		});// runOnUiThread
+		Log.v("testTextViewHello", (String) this.textView.getText());
+		assertEquals(this.textView.getText(), "Hello World, HelloAndroidActivity!");
+	}// testTextViewHello
+
+	Activity activity;
+	TextView textView;
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		this.activity = getActivity();
+		this.textView = (TextView) this.activity
+				.findViewById(R.id.textViewHello);
+		super.setUp();
+	}// setUp
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}// tearDown
+}// UsersActivityTestCase
+
